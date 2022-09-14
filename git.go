a943b7f3b3cfa17f0d9332005048d9640610102b
0xpeIpeI <hokumaru12@gmail.com>
2022-09-14 19:18:06 +0900

    add check file

--- git.go
+++ git.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strings"
 	"time"
 
 	"github.com/go-git/go-git/v5"
@@ -31,31 +28,4 @@ func SelfAllCommitLogs(s *git.Repository) object.CommitIter {
 		CheckIfError(err)
 	}
 	return c
-}
-
-// CheckIfError should be used to naively panics if an error is not nil.
-func CheckIfError(err error) {
-	if err == nil {
-		return
-	}
-
-	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
-	os.Exit(1)
-}
-
-func CheckArgs(arg ...string) {
-	if len(os.Args) < len(arg)+1 {
-		Warning("Usage: %s %s", os.Args[0], strings.Join(arg, " "))
-		os.Exit(1)
-	}
-}
-
-// Info should be used to describe the example commands that are about to run.
-func Info(format string, args ...interface{}) {
-	fmt.Printf("\x1b[34;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
-}
-
-// Warning should be used to display a warning
-func Warning(format string, args ...interface{}) {
-	fmt.Printf("\x1b[36;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
 }
